d0ec88b3e3820b4bebaf16de3b14f6c9794f54e6
Fernando Henrique Fernandes <nandohfernandes@gmail.com>
2019-11-06 10:10:22 -0300

    feat: add complecity yto models

--- DRL Nanodegree/Personal Exercises Solutios/4. Multi-Agent  Reinforcement Learning/p3_collab-compet/model.py
+++ DRL Nanodegree/Personal Exercises Solutios/4. Multi-Agent  Reinforcement Learning/p3_collab-compet/model.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=64):
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=64,
+                 fc3_units=32):
         """Initialize parameters and build model.
         Params
         ======
@@ -22,13 +23,15 @@ class Actor(nn.Module):
         self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.bn2 = nn.BatchNorm1d(fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.fc4 = nn.Linear(fc3_units, action_size)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.bn1(self.fc1(state)))
         x = F.relu(self.bn2(self.fc2(x)))
-        return torch.tanh(self.fc3(x))
+        x = F.relu(self.fc3(x))
+        return torch.tanh(self.fc4(x))
 
 
 class Critic(nn.Module):
